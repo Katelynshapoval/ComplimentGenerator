0897ee3a8d89dbac4c3d1677095c9c83960308c5
Kateryna <katelyn.shapoval@gmail.com>
2024-06-01 22:06:33 +0200

    first

---
+++ src/quote.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+
+function GenerateQuote() {
+  // State to store the current compliment
+  const [compliment, setCompliment] = useState("");
+
+  // List of compliments to choose from
+  const listOfCompliments = [
+    "Baby, I am so in love with you.",
+    "My love, you are the best thing that ever happened to me.",
+    "You're so beautiful it makes me nervous.",
+    "Fuck me.",
+    "I hope you know how beautiful you are.",
+    "You will never be difficult for me.",
+    "I'm the luckiest girl alive because I have you.",
+    "I am very proud of you.",
+    "One day, I really want us to say 'We made it' while living together in Italy.",
+    "No matter what happens in the future, I want to be by your side.",
+    "I absolutely adore your smile.",
+    "I still cannot that I have such a perfect girlfriend.",
+    "Fuck france.",
+    "I really want to kiss you, baby.",
+    "Calling you immediately makes my day better.",
+    "I love you.",
+    "I love everything about you - your body, your personality, your smile, your sense of humour.",
+    "No matter what you say about yourself, you will always be perfect to me.",
+  ];
+
+  // useEffect hook to set the initial compliment when the component mounts
+  useEffect(() => {
+    // Set a random compliment initially
+    setCompliment(getRandomCompliment(listOfCompliments));
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+  // Function to set a new random compliment
+  function makeCompliment() {
+    let randomCompliment = getRandomCompliment(listOfCompliments);
+    while (randomCompliment === compliment) {
+      randomCompliment = getRandomCompliment(listOfCompliments);
+    }
+    setCompliment(randomCompliment);
+  }
+
+  return (
+    <div className="parent">
+      <div class="panda">
+        <div class="ears"></div>
+        <div class="eyes">
+          <div class="pupil"></div>
+        </div>
+        <div class="eyes eyes-r">
+          <div class="pupil"></div>
+        </div>
+        <div className="nose"></div>
+      </div>
+      <div className="complimentBox">
+        {/* Display the current compliment */}
+        <div className="compliment">
+          <p>{compliment}</p>
+          {/* Button to change the compliment */}
+          <button className="changeQuoteButton" onClick={makeCompliment}>
+            Change
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Function to get a random compliment from the list
+function getRandomCompliment(compliments) {
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  return compliments[randomIndex];
+}
+
+export default GenerateQuote;
